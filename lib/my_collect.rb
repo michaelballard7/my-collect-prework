264ada60814db6972925e20567bf14400b1c58a7
Michael Ballard <ballard.michael9@gmail.com>
2018-05-09 02:42:27 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
 def my_collect(array)
   i = 0 
+  new_arr = [ ]
   while i < array.length 
-    new_array << array[i]
-    yield array[i]
+    yield new_array << array[i]
+    
     i += 1
   end
   new_array
